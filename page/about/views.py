07cb1515014d8e58105351d4f9d6cf7bc57d3f9b
BioUSik <is109-lavrov@urtt.ru>
2022-09-03 07:16:27 +0300

    initial create

---
+++ page/about/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+def homePageView(request):
+    return render(request, "home.html")
+
+def aboutPageView(request):
+    return render(request, "about.html")
